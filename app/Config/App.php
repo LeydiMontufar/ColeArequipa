3df730208363e37056a6e070d01d7a3481bc9c5d
Jaime supo <jaimesupo@gmail.com>
2022-11-26 14:53:47 -0500

    locaso

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/colearequipa/';
+    public $baseURL = 'http://localhost/ColeArequipa/';
 
     /**
      * --------------------------------------------------------------------------
