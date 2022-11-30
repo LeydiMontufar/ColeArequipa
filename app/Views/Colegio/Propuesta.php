910c241d651de4221dff7912d2bf51eb8db47292
Leydi Montufar <leydimontufar16@gmail.com>
2022-11-29 21:54:16 -0500

    el pepe2

--- app/Views/Colegio/Propuesta.php
+++ app/Views/Colegio/Propuesta.php
@@ -5,9 +5,9 @@
         <div class="container py-5">
             <div class="row align-items-center py-4">
                 <div class="col-md-6 text-center text-md-left">
-                    <h1 class="mb-4 mb-md-0 text-primary text-uppercase">AREA ESPIRITUAL</h1>
-                    <h3 class="display-3 text-white mb-md-4">Area espiritual</h3>
-                    <p class="m-0">El Colegio Ebenezer, como parte del plan de formación integral, realiza las actividades espirituales en bien de la formación de nuestros estudiantes acorde con el desarrollo del niño y del adolescente.</p>
+                    <h1 class="mb-4 mb-md-0 text-primary text-uppercase"></h1>
+                    <h3 class="display-3 text-white mb-md-4">Nuestra Propuesta</h3>
+                    <p class="m-0"></p>
                 </div>
                 <div class="col-md-6 text-center text-md-right">
                     <div class="d-inline-flex align-items-center">
@@ -19,5 +19,96 @@
             </div>
         </div>
     </div>
+<!-- hhhhhhhhh -->
+<div class="container-fluid bg-light pt-5">
+        <div class="container py-5">
+            <div class="row justify-content-center">
+            </div>
+            <div class="row pb-3">
+                <div class="col-md-4 mb-4">
+                    <div class="card border-0 mb-2">
+                        <img class="card-img-top" src="img/blog-1.jpg" alt="">
+                        <div class="card-body bg-white p-4">
+                            <div class="d-flex align-items-center mb-3">
+                                <a class="btn btn-primary" href=""><i class="fa fa-link"></i></a>
+                                <h5 class="m-0 ml-3 text-truncate"></h5>
+                            </div>
+                            <p>Malla Curricular.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card border-0 mb-2">
+                        <img class="card-img-top" src="img/blog-2.jpg" alt="">
+                        <div class="card-body bg-white p-4">
+                            <div class="d-flex align-items-center mb-3">
+                                <a class="btn btn-primary" href=""><i class="fa fa-link"></i></a>
+                                <h5 class="m-0 ml-3 text-truncate"></h5>
+                            </div>
+                            <p>Reglamento Interno.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card border-0 mb-2">
+                        <img class="card-img-top" src="img/blog-3.jpg" alt="">
+                        <div class="card-body bg-white p-4">
+                            <div class="d-flex align-items-center mb-3">
+                                <a class="btn btn-primary" href=""><i class="fa fa-link"></i></a>
+                                <h5 class="m-0 ml-3 text-truncate"></h5>
+                            </div>
+                            <p>Otros Documentos.</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
 
+<div class="container-fluid py-5">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-lg-6 pr-lg-5">
+                    <h6 class="text-primary font-weight-normal text-uppercase mb-3"></h6>
+                    <h1 class="mb-4 section-title">PROPUESTA PEDAGOGICA</h1>
+                    <a href="" class="btn btn-primary mt-3 py-2 px-4">EBENEZER</a>
+                </div>
+                <div class="col-lg-6 p-0 pt-5 pt-lg-0">
+                    <div class="owl-carousel service-carousel position-relative">
+                        <div class="d-flex flex-column text-center bg-light mx-3 p-4">
+                            <h3 class="flaticon-bedroom display-3 font-weight-normal text-primary mb-3"></h3>
+                            <h5 class="mb-3">Inteligencias Múltiples</h5>
+                            <p class="m-0">Basado en el desarrollo del potencial de las inteligencias múltiples de manera integral en las diferentes áreas curriculares, haciendo uso de la mejor manera en las competencias y capacidades que exige la sociedad del conocimiento.</p>
+                        </div>
+                        <div class="d-flex flex-column text-center bg-light mx-3 p-4">
+                            <h3 class="flaticon-kitchen display-3 font-weight-normal text-primary mb-3"></h3>
+                            <h5 class="mb-3">Desarrolllo de Habilidades</h5>
+                            <p class="m-0">Buscamos que los alumnos desarrolllen habilidades que les permitan ser críticos, creativos, tomar decisiones y solucionar problemas. Esto implica una metodología en la que el alumno sea el protagonista de su propio aprendizaje y sea él quien construya sus conocimientos.</p>
+                        </div>
+                        <div class="d-flex flex-column text-center bg-light mx-3 p-4">
+                            <h3 class="flaticon-bathroom display-3 font-weight-normal text-primary mb-3"></h3>
+                            <h5 class="mb-3">Liderazgo</h5>
+                            <p class="m-0">Fomentamos el liderazgo de nuestros estudiantes a través del desarrollo de diferentes actividades, donde demuestran sus conocimientos en la búsqueda de solucionar problemas de nuestro contexto social.</p>
+                        </div>
+                        <div class="d-flex flex-column text-center bg-light mx-3 p-4">
+                            <h3 class="flaticon-bathroom display-3 font-weight-normal text-primary mb-3"></h3>
+                            <h5 class="mb-3">Tecnología</h5>
+                            <p class="m-0"> Dotamos a cada uno de nuestros estudiantes de las herramientas tecnológicas necesarias, de manera que ellos puedan reconocer y valorar herramientas de apoyo a su proceso formativo y de estudio.</p>
+                        </div>
+                        <div class="d-flex flex-column text-center bg-light mx-3 p-4">
+                            <h3 class="flaticon-bathroom display-3 font-weight-normal text-primary mb-3"></h3>
+                            <h5 class="mb-3">Investigación</h5>
+                            <p class="m-0">Somos una comunidad en busca de la Verdad, por lo que impulsamos el trabajo investigativo en todos nuestros estudiantes, para ello se plantean investigaciones abiertas, dirigidas, viajes de estudio, etc., que permitan a todos los estudiantes vivir la experiencia del proceso de investigación y de demostrarlo mediante un producto acorde a sus posibilidades.</p>
+                        </div>
+                        <div class="d-flex flex-column text-center bg-light mx-3 p-4">
+                            <h3 class="flaticon-bathroom display-3 font-weight-normal text-primary mb-3"></h3>
+                            <h5 class="mb-3">Trascendencia</h5>
+                            <p class="m-0">Relacionando los temas y actividades trabajadas con el sentido de la vida y el éxito; buscando el desarrollo de habilidades sociales de cada uno de nuestros estudiantes con la práctica de valores para poder conocerse, aceptarse y superarse; reconociendo a Dios como el centro de nuestra vida y nuestra felicidad.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?= $this->endSection()?>
